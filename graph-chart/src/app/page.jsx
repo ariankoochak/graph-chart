d71c6a8c050e0afda1d43237c3f740ad39b7adba
Arian <arian.koochak@protonmail.com>
2025-02-15 15:27:28 +0330

    add style for button & add button in cas management container

--- graph-chart/src/app/page.jsx
+++ graph-chart/src/app/page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function page() {
   return (
     <section>
-      <div className="csv-management-container"></div>
+      <div className="csv-management-container">
+        <button className='upload-csv-btn'>Upload CSV File</button>
+      </div>
       <div className="graph-chart-container"></div>
       <div className="node-details"></div>
     </section>
