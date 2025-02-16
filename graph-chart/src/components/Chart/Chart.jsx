45184f1bf9c969d0f3f9fd26b717761ae04f2964
Arian <arian.koochak@protonmail.com>
2025-02-17 00:55:21 +0330

    --fix : remove unused highcharts datas & pass onclick to out of component

--- graph-chart/src/components/Chart/Chart.jsx
+++ graph-chart/src/components/Chart/Chart.jsx
@@ -6,7 +6,7 @@ import { startTransition, useActionState, useEffect, useState } from "react";
 import DrawGraph from "../DrawGraph/DrawGraph";
 
 
-export default function Chart({ csv }) {
+export default function Chart({ csv , onNodeClicked}) {
     const [warning, setWarning] = useState("");
     const [err, setErr] = useState("");
 
@@ -14,122 +14,33 @@ export default function Chart({ csv }) {
         prepareGraphForHighCharts
     );
     useEffect(() => {
-        if (csv.length > 0) {
-            startTransition(() => {
-                formAction(csv);
-            });
-        }
-    }, [csv]);
-
-    const [options, setOptions] = useState(null);
-    const [Highcharts, setHighcharts] = useState(null);
-
-    const loadHighChartsLibs = () => {
-        if (typeof window !== "undefined") {
-            import("highcharts")
-                .then((mod) => {
-                    const HighchartsLib = mod.default || mod;
-                    setHighcharts(HighchartsLib);
-                    return import("highcharts/modules/networkgraph");
-                })
-                .then((netMod) => {
-                    if (Highcharts) {
-                        (netMod.default || netMod)(Highcharts);
-                    }
-                })
-                .catch((err) =>
-                    console.error("Error loading Highcharts modules:", err)
-                );
-        }
-        if (typeof window !== "undefined") {
-            import("highcharts")
-                .then((mod) => {
-                    const HighchartsLib = mod.default || mod;
-                    setHighcharts(HighchartsLib);
-                    return import("highcharts/modules/boost");
-                })
-                .then((boostMod) => {
-                    if (Highcharts) {
-                        (boostMod.default || boostMod)(Highcharts);
-                    }
-                })
-                .catch((err) =>
-                    console.error("Error loading Highcharts modules:", err)
-                );
-        }
-    };
-    useEffect(() => {
-        loadHighChartsLibs();
-    }, []);
-
-    const initializeGraphObject = () => {
         try {
-            if (graph) {
-                prepareOptionForHighCharts(graph);
-                setErr("");
-                setWarning("");
+            if (csv.length > 0) {
+                startTransition(() => {
+                    formAction(csv);
+                });
             }
         } catch (error) {
-            errorHandlingInInitializeGraphObject(error.status);
+            errorHandlingForGraph(error.status);
         }
-    };
+        
+    }, [csv]);
 
-    const errorHandlingInInitializeGraphObject = (errorStatus) => {
-        switch (errorStatus) {
-            case 404:
-                setWarning("فایل csvیی انتخاب نشده");
-                break;
-            case 422:
-                setErr("لطفا فایل csv خود را با فرمت صحیح آپلود کنید");
-                break;
-            default:
-                setErr("مشکلی پیش آمده - ارور نامشخص");
-        }
-    };
-
-    const prepareOptionForHighCharts = (graph) => {
-        if (Highcharts && graph) {
-            setOptions({
-                boost: {
-                    useGPUTranslations: true,
-                    usePreallocated: true,
-                },
-                chart: {
-                    type: "networkgraph",
-                    marginTop: 80,
-                    height : '96.5%',
-                },
-                title: {
-                    text: "Graph",
-                },
-                plotOptions: {
-                    networkgraph: {
-                        keys: ["from", "to"],
-                    },
-                },
-                series: [
-                    {
-                        marker: {
-                            radius: 5,
-                        },
-                        dataLabels: {
-                            enabled: true,
-                            linkFormat: "",
-                            allowOverlap: true,
-                        },
-                        data: graph.edges.slice(0, 800),
-                    },
-                ],
-            });
-        }
-    };
-
-    useEffect(() => {
-        initializeGraphObject();
-    }, [graph]);
+     const errorHandlingForGraph = (errorStatus) => {
+         switch (errorStatus) {
+             case 404:
+                 setWarning("فایل csvیی انتخاب نشده");
+                 break;
+             case 422:
+                 setErr("لطفا فایل csv خود را با فرمت صحیح آپلود کنید");
+                 break;
+             default:
+                 setErr("مشکلی پیش آمده - ارور نامشخص");
+         }
+     };
 
     if (err !== "") return <span className="error">{err}</span>;
     if (warning !== "") return <span className="warning">{warning}</span>;
     if (isPending) return <span className="success">در حال بارگذاری...</span>;
-    if (graph) return <DrawGraph graph={graph}/>;
+    if (graph) return <DrawGraph graph={graph} onNodeClick={onNodeClicked}/>;
 }
